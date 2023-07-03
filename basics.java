8e921195c8306fce275ec2e7a263aa8c9b302c54
Saurabh Bansal <saurabhbansal2443@gmail.com>
2023-07-03 17:07:13 +0530

    stack started

---
+++ basics.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+public class basics{
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+
+        st.push(56);
+        st.push(58);
+        st.push(67);
+
+        System.out.println(st.size());
+
+        st.pop();
+
+        System.out.println(st.peek());
+
+        st.pop();
+
+        // System.out.println(st.peek());
+
+        // st.pop();
+
+        // System.out.println(st.peek());
+    }
+}
